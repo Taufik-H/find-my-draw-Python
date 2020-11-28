968d2941fcaa55718e1563a1bf047240d5c716cb
Taufik <taufikrpl1@gmail.com>
2020-11-28 09:06:25 +0700

    pengenalan objek

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageTk, ImageDraw
 
 window =tk.Tk()
 
-img = Image.new(mode="1", size=(500, 500), color=255 )
+img = Image.new(mode="1", size=(500, 500), color=0 )
 tkimage = ImageTk.PhotoImage(img)
 canvas = tk.Label(window, image=tkimage)
 canvas.pack()
@@ -14,7 +14,7 @@ draw = ImageDraw.Draw(img)
 def draw_image(event):
     global titik_akhir,tkimage
     titik_gerak = (event.x, event.y)
-    draw.line([titik_akhir, titik_gerak], fill=0, width=1)
+    draw.line([titik_akhir, titik_gerak], fill=255, width=10)
     titik_akhir = titik_gerak
     tkimage = ImageTk.PhotoImage(img)
     canvas['image'] = tkimage
@@ -27,14 +27,35 @@ def start_draw(event):
 
 def hapus_line(event):
     global tkimage, img, draw
-    img = Image.new(mode="1", size=(500, 500), color=255 )
+    img = Image.new(mode="1", size=(500, 500), color=0 )
     draw = ImageDraw.Draw(img)
     tkimage = ImageTk.PhotoImage(img)
     canvas['image'] = tkimage
     canvas.pack()
 
+smile = 0
+sad = 0
+angry =0
+
+def save_image(event):
+    global smile,sad,angry
+    img_board = img.resize((50,50))
+
+    if(event.char == "q"):
+        img_board.save(f"smile/{smile}.png")
+        smile += 1
+    elif (event.char == "w"):
+        img_board.save(f"sad/{sad}.png")
+        sad += 1
+    elif (event.char == "e"):
+        img_board.save(f"angry/{angry}.png")
+        angry += 1
+        
+
+
 window.bind("<B1-Motion>", draw_image)
 window.bind("<ButtonPress-1>", start_draw)
 window.bind("<ButtonPress-3>", hapus_line)
+window.bind("<Key>", save_image)
 window.mainloop()
 
